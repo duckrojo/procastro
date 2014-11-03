4d841c0c16ffc44560e879ab25c038a9888efab7
Francisca Concha <fran.concha.r@gmail.com>
2014-11-03 06:18:11 +0000

    Fixed Observer notifications, added dictionaries to both Observable and Observer

--- fran/Observer/Observer.py
+++ fran/Observer/Observer.py
@@ -7,11 +7,13 @@ class Observer(object):
         self.name = name
         self.elements = elements
 
-    def update(observable, arg):
+    def update(self, obs, label, value):
         """Called when the observed object is
         modified. You call an Observable object's
         notifyObservers method to notify all the
         object's observers of the change."""
+        self.elements[label] = value
+        print("Eye " + str(self.name) + " has changed data " + str(label))
         pass
 
 class Observable(Synchronization):
@@ -29,15 +31,14 @@ class Observable(Synchronization):
         """ Adds new Observer of Observable """
         if observer not in self.obs:
             self.obs.append(observer)
-            print("New Observer added.")
-            #print self.obs
+            print("New Observer added: " + str(observer.name))
 
     def deleteObserver(self, observer, name):
         """ Deletes Observer """
         print("Observer " + name + " has been deleted.")
         self.obs.remove(observer)
 
-    def notifyObservers(self, label = None, arg = None):
+    def notifyObservers(self, label, value, arg = None):
         """ If 'changed' indicates that this object
         has changed, notify all its observers, then
         call clearChanged(). Each Observer has its
@@ -49,14 +50,18 @@ class Observable(Synchronization):
             if not self.changed: return
             # Make a local copy in case of synchronous
             # additions of observers:
+            #print self.obs
             localArray = self.obs[:]
             self.clearChanged()
         finally:
             self.mutex.release()
         # Updating is not required to be synchronized:
+        print localArray
         for observer in localArray:
-            print(str(observer) + " has been updated.")
-            observer.update(self, arg)
+            if label in observer.elements:
+                print("Observer has been updated.")
+                print label, value
+                observer.update(self, label, value)
 
     def deleteObservers(self):
         print("All observers have been deleted.")
--- fran/sharedObject.py
+++ fran/sharedObject.py
@@ -4,7 +4,7 @@ __author__ = 'fran'
 from Observer import Observer, Observable
 
 
-class sharedObject(Observable):  # TODO Ojo aca, no se si deba inheritar AstroFile
+class sharedObject(Observable):
     def __init__(self, data, elements):
         """ Ojo: data puede ser un Astrofile o un Scipy array,
             por eso no especifico por separado el header + data.
@@ -14,14 +14,10 @@ class sharedObject(Observable):  # TODO Ojo aca, no se si deba inheritar AstroFi
         """
         Observable.__init__(self)
         self.data = data
-        self.elements = elements
+        self.shared = elements
         self.isOpen = 0  # Se inicializa "cerrado"
         # Creo los Notifiers para los distintos elementos del diccionario
-        self.shareDict = {}
-        for elem in self.elements:
-            if self.elements[elem]:
-                # Si quiero compartir ese elemento, agrego un notificador
-                self.shareDict[elem] = sharedObject.ChangeNotifier(self)
+        #self.changeNotifier = sharedObject.ChangeNotifier(self)
         self.openNotifier = sharedObject.OpenNotifier(self)
 
     def open(self):  # Abre "conexion"
@@ -35,27 +31,36 @@ class sharedObject(Observable):  # TODO Ojo aca, no se si deba inheritar AstroFi
         #self.closeNotifier.notifyObservers()
         self.openNotifier.close()
 
-    def setData(self, newdata):  # Cambia los datos
-        self.data = newdata
-        for n in self.shareDict:
-            self.shareDict[n].openNotifier.notifyObservers()
-        self.openNotifier.notifyObservers()
+    def changeData(self, label, newdata):  # Cambia los datos
+        print("Observable data has changed.")
+        print("Old data: " + str(self.shared))
+        self.shared[label] = newdata
+        print("New data: " + str(self.shared))
+        #print self.obs
+        #  Notifico a todos los Observers, ellos filtran si les sirve la info
+        #self.changeNotifier.notifyObservers()
+        self.notifyObservers(label, newdata)
+        #for n in self.shareDict:
+        #    self.shareDict[n].openNotifier.notifyObservers()
+        #self.openNotifier.notifyObservers()
 
     #def closing(self): return self.closeNotifier
 
     # TODO OJO CON ESTO
-    class ChangeNotifier(Observable):
+    """class ChangeNotifier(Observable):
         def __init__(self, outer):
             Observable.__init__(self)
             self.outer = outer
-            self.alreadyOpen = 0
-        def notifyObservers(self, label):
-            if self.outer.isOpen and not self.alreadyOpen:
-                self.setChanged()
-                Observable.notifyObservers(self)
-                self.alreadyOpen = 1
-        def close(self):
-            self.alreadyOpen = 0
+            self.alreadyOpen = 0"""
+    def notifyObservers(self, label, value):
+        print("Notification sent to Observers.")
+        #if self.outer.isOpen and not self.alreadyOpen:
+        self.setChanged()
+        #print self.obs
+        Observable.notifyObservers(self, label, value)
+        self.alreadyOpen = 1
+        """def close(self):
+            self.alreadyOpen = 0"""
 
     class OpenNotifier(Observable):
         def __init__(self, outer):
@@ -66,7 +71,7 @@ class sharedObject(Observable):  # TODO Ojo aca, no se si deba inheritar AstroFi
             if self.outer.isOpen and \
             not self.alreadyOpen:
                 self.setChanged()
-                Observable.notifyObservers(self)
+                Observable.notifyObservers(self, 0, 0)
                 self.alreadyOpen = 1
         def close(self):
             self.alreadyOpen = 0
@@ -80,7 +85,7 @@ class sharedObject(Observable):  # TODO Ojo aca, no se si deba inheritar AstroFi
             if not self.outer.isOpen and \
             not self.alreadyClosed:
                 self.setChanged()
-                Observable.notifyObservers(self)
+                Observable.notifyObservers(self, 0, 0)
                 self.alreadyClosed = 1
         def open(self):
             alreadyClosed = 0
@@ -90,7 +95,7 @@ class sharedObject(Observable):  # TODO Ojo aca, no se si deba inheritar AstroFi
 class Eye(Observer):
     def __init__(self, name, elements):
         Observer.__init__(self, name, elements)
-        self.elements = elements
+        #self.elements = elements
         self.openObserver = Eye.OpenObserver(self)
         self.closeObserver = Eye.CloseObserver(self)
     # An inner class for observing openings:
@@ -108,15 +113,14 @@ class Eye(Observer):
 
 
 # Diccionarios con elementos a compartir
-shared_elem = {'fulldata': True, 'xlim': False, 'ylim': True}
-eye_elem = {'fulldata': False, 'xlim': True, 'ylim': False}
+# Dummy values
+shared_elem = {'fulldata': 10, 'xlim': 5, 'ylim': 7}
+eye_elem = {'xlim': 0}
 
 s = sharedObject(1, shared_elem)
-#e1 = Eye("Eye1", eye_elem)
+e1 = Eye("Eye1", eye_elem)
 #s.openNotifier.addObserver(e1.openObserver)
 s.open()
-print s.obs
-e1 = Eye("Eye1", eye_elem)
 s.addObserver(e1)
-s.setData = 2
-print s.obs
+s.changeData('ylim', 2)
+print e1.elements
