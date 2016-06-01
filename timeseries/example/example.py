4caeabf98964b1e301d914d33919be035be90d5a
Francisca Concha <fran.concha.r@gmail.com>
2016-06-01 21:05:07 +0000

    Added operations between TimeSerie channels, plot radial profile

--- timeseries/example/example.py
+++ timeseries/example/example.py
@@ -7,6 +7,7 @@ dark = AstroFile("./data/dark.fits")
 flat = AstroFile("./data/flat.fits")
 
 target_coords = [[570, 269], [436, 539]] # Coordinates of 1 target and 1 reference
+# Coordinates in y, x format!!!
 labels = ["Target", "Ref1"]
 aperture = 12  # Aperture
 sky = [16, 20]  # Sky
@@ -16,28 +17,29 @@ stamp_rad = 30  # Square radius for stamp
 ron = 1.
 gain = 1.
 
-# Initialize Photometry object. It calculate_stamps=True, stamps for photometry will be calculated upon
+# ======= INITIALIZING PHOTOMETRY OBJECT =======
+# Initialize Photometry object. If calculate_stamps=True, stamps for photometry will be calculated upon
 # initialization and get_stamps does not have to be explicitely called by the user
-
 phot = Photometry(raw, aperture=aperture, sky=sky, mdark=dark, mflat=flat,
                   calculate_stamps=True, target_coords=target_coords, stamp_rad=stamp_rad,
                   labels=labels, gain=gain, ron=ron)
 
-.imshowz()
-plot_radialprofile()
-showstamp
+# Plot radial profile of targets; can specify targets with labels
+phot.plot_radialprofile()
+phot.plot_radialprofile(targets=['Ref1'])
+# Can also specify for which frame to plot
+phot.plot_radialprofile(targets=['Ref1', 'Target'], frame=2)
 
-ts_cpu = phot.photometry(apert,sky)
-#apert & sky are optional, in which , they 
+phot.showstamp()
 
+# ======= EXECUTING PHOTOMETRY =======
+# Aperture and sky are optional, they can be given when initializing Photometry object or when running
+# the photometry function. The GPU flag is also optional (default is gpu=False). The results are
+# TimeSerie.TimeSeries objects!
+ts_cpu = phot.photometry(aperture=aperture, sky=sky)
 ts_gpu = phot.photometry(gpu=True)
 
 print(ts_cpu.__class__)  # Just to show that it is a TimeSerie.TimeSeries object
-
-ts_cpu.plot()
-ts_gpu.plot()
-
-ts_cpu[1]/ts_cpu[-1]
 
 # Can be run without reducing, ie without bias, dark, flat
 phot2 = Photometry(raw, aperture, sky, mdark=None, mflat=None,
@@ -56,3 +58,46 @@ phot3 = Photometry(sci_stamps, aperture, sky, mdark=dark, mflat=flat, calculate_
                    stamp_coords=stamp_coords, epoch=epoch, labels=labels, gain=gain, ron=ron)
 
 ts_cpu_3 = phot3.photometry()
+
+# ======= OPERATIONS FOR TIMESERIES =======
+# Plot photometry results; ts_cpu and ts_gpu are TimeSerie.TimeSeries objects
+ts_cpu.plot()
+ts_gpu.plot()
+
+# Access to light curves for different targets
+res_ref1_cpu = ts_cpu[1]  # Returns the light curve of the 2 nd target object as scipy array
+res_ref1_gpu = ts_gpu['Ref1']  # Returns the light curve of the target assigned the 'REF1' label
+
+# Get the error corresponding to the data channel
+ref1_error_cpu = ts_cpu.get_error(1)
+target_error_cpu = ts_cpu.get_error('Target')
+
+# TimeSerie channels can be grouped to perform operations between them.
+# Only two groups are allowed for each TimeSerie object. The default grouping is
+# the first target in a group, and the rest of the targets in another group. To group channels
+# differently, a mask must be applied (error channels are also grouped according to the mask):
+ts_cpu.set_group([1, 1, 0, 1, 1])  # Groups channels 0, 1, 3, and 4 in one group, 2 in another
+ts_gpu.set_group([1, 1, 1, 0, 0])  # Groups channels 0, 1, and 2 in one group, 3 and 4 in another
+
+# Retrieve grouped channels:
+group1_cpu = ts_cpu.group1()
+group1_cpu_errors = ts_cpu.errors_group1()
+
+# Calculate mean or median of a group
+ts_cpu.mean(1)
+ts_gpu.median(2)
+
+# Results of these operations are stored in the "hidden" channels ts[-group_id]
+res_cpu_mean = ts_cpu[-1]
+res_gpu_median = ts_gpu[-2]
+
+# Operations can be performed between individual channels. The results of these operations
+# are NOT stored in the TimeSeries object, only returned
+div = ts_cpu[1]/ts_cpu[0]
+added = ts_gpu[0] + ts_gpu['Ref1']
+
+
+
+
+
+
