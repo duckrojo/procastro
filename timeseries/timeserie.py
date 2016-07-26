df163c51536c4c52759d0e832e3ca9f82f8b50fd
Patricio Rojo <pato@das.uchile.cl>
2016-07-26 07:48:46 +0000

    changed combination on-the-fly upon negative indices, including error propagation, indexing accepts string as well, grouping on its own flag file, plot() and plot_ratio() working

--- timeseries/timeserie.py
+++ timeseries/timeserie.py
@@ -1,4 +1,3 @@
-
 #
 #
 # Copyright (C) 2016 Francisca Concha, Patricio Rojo
@@ -18,160 +17,111 @@
 # Boston, MA  02110-1301, USA.
 #
 #
-import copy
 import scipy as sp
-import warnings
-import numpy as np
 import dataproc as dp
 from datetime import datetime
 from matplotlib import dates
-
+import matplotlib.pyplot as plt
 
 __author__ = 'fran'
 
+
 class TimeSeries(object):
-
     def __repr__(self):
-        return "<timeseries object with {channels} channels of {size} elements (Extras={extras}). {ret}>".format(channels=len(self.channels), size=len(self.channels[0]), extras=self.extras.keys(),
-                                                                                                                 ret=self._extra_call is not None and "Returning extra {ex}".format(ex=self._extra_call) or "")
+        return "<timeseries object with {channels} channels of {size} " \
+               "elements (Extras={extras}).>".format(channels=len(self.channels),
+                                                     size=len(self.channels[0]),
+                                                     extras=self.extras.keys(), )
 
+    def __init__(self, data, errors=None, labels=None, epoch=None, extras=None,
+                 grouping='mean'):
 
-    def __init__(self, data, errors=None, labels=None, epoch=None, extras=None):
+        self.combine = {}
+        self.grouping_with(grouping)
+        self.channels = [sp.array(d) for d in
+                         data]  # [[target1_im1, target1_im2, ...], [target2_im1, target2_im2, ...]]
 
-        self.channels = [sp.array(d) for d in data]  # [[target1_im1, target1_im2, ...], [target2_im1, target2_im2, ...]]
-
-
-        self.group = [True] + [False]*(len(data)-1)
         # Default grouping: 1st coordinate is 1 group, all other objects are another group
+        self.groups = sp.ones(len(self.channels)) + 1
+        self.groups[0] = 1
 
-
-        self.labels = {}
+        self.labels = []
         if labels is not None:
-            self.labels = self.set_labels(labels)  # Dictionary for names?
-        #else:
-        #    self.labels = {}
+            self.labels = labels
 
         if extras is None:
             extras = {}
         elif not isinstance(extras, dict):
             raise TypeError("extras must be a dictionary")
         else:
-            if False in [isinstance(v,(list, )) for v in extras.values()]:
+            if False in [isinstance(v, (list,)) for v in extras.values()]:
                 raise TypeError("Each element of 'extras' dictionary must be a list")
 
-        self.extras=extras
-        self._extra_call=None
+        self.extras = extras
+        self._extra_call = None
 
         if errors is None:
             errors = []
         self.extras['errors'] = [sp.array(e) for e in errors]
         # [[error_target1_im1, error_target1_im2, ...], [error_target2_im1, error_target2_im2, ...]]
 
-
-        self.channels.append([])  # Group 1 operation result; is overwritten every time a new op is defined
-        for v in self.extras.values():
-            v.append([])
-
-        self.channels.append([])  # Group 2 operation result; is overwritten every time a new op is defined
-        for v in self.extras.values():
-            v.append([])
-
         self.epoch = epoch
 
     def __call__(self, **kwargs):
-        if len(kwargs)!=1:
+        if len(kwargs) != 1:
             raise ValueError("Only one argument in the form 'field=target' must be given to index it")
-        k,v = kwargs.items()[0]
-
-        if isinstance(v, str):
-            item = self.ids.index(v)
+        k, v = kwargs.items()[0]
 
         if k in self.extras:
-            return self.extras[k][v]
+            ret_extra = self.extras[k]
         else:
             raise ValueError("Cannot find requested field '{0}'".format(k))
 
+        if isinstance(v, str):
+            item = self.labels.index(v)
+        else:
+            item = v
+
+        if isinstance(item, int):
+            if item < 0:
+                if k == 'errors':
+                    if not (self.combine['name'] == 'mean' or self.combine['name'] == 'median'):
+                        raise NotImplementedError("Only know how to compute error of median- or mean-combined groups.")
+                    sample = sp.array(self.groups) == abs(item)
+                    sigma = sp.array(ret_extra)[sample]
+                    variance = (sigma ** 2).sum(0)
+                    return sp.sqrt(variance) / len(sample)
+                else:
+                    raise NotImplementedError("Only combined errors have been implemented")
+            else:
+                return ret_extra[item]
+        else:
+            raise TypeError("unrecognized channel specification ({}) for "
+                            "extra '{}'".format(item, k))
 
     def __getitem__(self, item):
         """To recover errors or any of the other extras try: ts('error')[0] ts['centers_xy']['Target'], etc"""
         # This is so I can do ts[0]/ts[2] and it works directly with the channels!
 
-
-
         if isinstance(item, str):
-            item = self.ids.index(item)
+            item = self.labels.index(item)
+        elif item < 0:
+            return self.combine['op'](sp.array(self.channels)[sp.array(self.groups) == abs(item)],
+                                      **(self.combine['args']))
 
-        ret = self.channels[item]
+        return self.channels[item]
 
-        return ret
-
-
-    def get_error(self, item):
-        return self(errors=item)
-
-    def group1(self):
-        return sp.array(self.channels[:-2])[sp.array(self.group)]
-
-    def group2(self):
-        return sp.array(self.channels[:-2])[sp.array(self.group)==False]
-
-    def set_group(self, new_group):  # Receives pe [0 1 0 0 1 0] and that is used to define 2 groups
-        self.group = new_group
-
-    def errors_group1(self):
-        return [self(errors=lab) for lab,g in zip(self.labels, self.group) if g]
-
-    def errors_group2(self):
-        return [self(errors=lab) for lab,g in zip(self.labels, self.group) if not g]
-
-    def set_labels(self, ids):
-        self.ids = ids
-        for i in range(len(self.ids)):
-            self.labels[self.ids[i]] = self.channels[i]
-        return self.labels
-
-    def set_epoch(self, e):
-        self.epoch = e
-
-    def mean(self, group_id):
-        if group_id > 2:
-            warnings.warn("group_id must be 1 or 2 only. Group 2 will be used as default.")
-            group = self.group2()
-            g_errors = self.errors_group2()
-        elif group_id == 1:
-            group = self.group1()
-            g_errors = self.errors_group1()
+    def grouping_with(self, op):
+        self.combine['name'] = op
+        if op == 'mean':
+            self.combine['op'] = sp.mean
+            self.combine['prm'] = {'axis': 0}
+        elif op == 'median':
+            self.combine['op'] = sp.median
+            self.combine['prm'] = {'axis': 0}
         else:
-            group = self.group2()
-            g_errors = self.errors_group2()
+            raise ValueError("Unrecognized combine operation '{}'".format(op))
 
-        self.channels[-group_id] = sp.mean(group, axis=0)
-        err = np.zeros((1, len(g_errors[0])))
-        for i in range(len(g_errors)):
-            err += np.divide(g_errors[i]/group[i])**2
-        self('errors')[-group_id] = np.sqrt(err)
-
-        return self.channels[-group_id]
-
-    def median(self, group_id):
-        if group_id > 2:
-            warnings.warn("group_id must be 1 or 2 only. Group 2 will be used as default.")
-            group = self.group2()
-            g_errors = self.errors_group2()
-        elif group_id == 1:
-            group = self.group1()
-            g_errors = self.errors_group1()
-        else:
-            group = self.group2()
-            g_errors = self.errors_group2()
-
-        self.channels[-group_id] = sp.median(group, axis=0)
-        err = np.zeros((1, len(g_errors[0])))
-        for i in range(len(g_errors)):
-            err += np.divide(g_errors[i]/group[i])**2
-        self.errors[-group_id] = np.sqrt(err)
-
-        return self.channels[-group_id]
 
     def plot(self, label=None, axes=None):
         """Display the timeseries data: flux (with errors) as function of mjd
@@ -181,39 +131,113 @@ class TimeSeries(object):
 
         :rtype: None (and plot display)
         """
-        import dataproc as dp
-        from datetime import datetime
-        from matplotlib import dates
+        # import dataproc as dp
+        # from datetime import datetime
+        # from matplotlib import dates
+        #
+        # date_epoch = [datetime.strptime(e, "%Y-%m-%dT%H:%M:%S.%f") for e in self.epoch]
+        # newepoch = [dates.date2num(dts) for dts in date_epoch]
+        # #newepoch = self.epoch
 
-        date_epoch = [datetime.strptime(e, "%Y-%m-%dT%H:%M:%S.%f") for e in self.epoch]
-        newepoch = [dates.date2num(dts) for dts in date_epoch]
-        #newepoch = self.epoch
-
-        fig, ax, epoch = dp.axesfig_xdate(axes, newepoch)
+        # fig, ax, epoch = dp.axesfig_xdate(axes, newepoch)
+        fig, ax = dp.figaxes(axes)
 
         if label is None:
-            disp = self.labels.keys()
+            disp = self.labels
         else:
             disp = [label]
 
-        # TODO check yerr
         for lab in disp:
-            if self.__getitem__(lab, error=True) is None:
-                yerr = None
-            else:
-                yerr = self.__getitem__(lab, error=True)
-
-            ax.errorbar(epoch,
-                        self.labels[lab],
-                        yerr=yerr,
-                        marker="o",
-                        label=lab)
+            ax.errorbar(self.epoch, self[lab], yerr=self(errors=lab),
+                        marker="o", label=lab)
 
         ax.set_title("Timeseries Data")
         ax.set_xlabel("MJD")
         ax.set_ylabel("Flux")
 
         ax.legend()
-        import matplotlib.pyplot as plt
         plt.show()
-        #return
+
+    def plot_ratio(self, label=None, axes=None):
+        fig, ax = dp.figaxes(axes)
+
+        ratio = self[-1] / self[-2]
+        ratio_error = ratio * sp.sqrt((self(errors=-1) / self[-1]) ** 2 +
+                                      (self(errors=-2) / self[-2]) ** 2)
+        ax.errorbar(self.epoch, ratio, yerr=ratio_error,
+                    marker="o")
+
+        labs = [', '.join(sp.array(self.labels)[sp.array(self.groups) == grp]) for grp in [1, 2]]
+        ax.set_title("Flux Ratio: \{{}\}/\{{}\}".format(labs[0], labs[1]))
+        ax.set_xlabel("MJD")
+        ax.set_ylabel("Flux Ratio")
+
+        plt.show()
+
+        # def get_error(self, item):
+        #     return self(errors=item)
+        #
+        # def group1(self):
+        #     return sp.array(self.channels[:-2])[sp.array(self.group)]
+        #
+        # def group2(self):
+        #     return sp.array(self.channels[:-2])[sp.array(self.group)==False]
+        #
+        # def set_group(self, new_group):  # Receives pe [0 1 0 0 1 0] and that is used to define 2 groups
+        #     self.group = new_group
+        #
+        # def errors_group1(self):
+        #     return [self(errors=lab) for lab,g in zip(self.labels, self.group) if g]
+        #
+        # def errors_group2(self):
+        #     return [self(errors=lab) for lab,g in zip(self.labels, self.group) if not g]
+        #
+        # def set_labels(self, ids):
+        #     self.ids = ids
+        #     for i in range(len(self.ids)):
+        #         self.labels[self.ids[i]] = self.channels[i]
+        #     return self.labels
+        #
+        # def set_epoch(self, e):
+        #     self.epoch = e
+        #
+        # def mean(self, group_id):
+        #     if group_id > 2:
+        #         warnings.warn("group_id must be 1 or 2 only. Group 2 will be used as default.")
+        #         group = self.group2()
+        #         g_errors = self.errors_group2()
+        #     elif group_id == 1:
+        #         group = self.group1()
+        #         g_errors = self.errors_group1()
+        #     else:
+        #         group = self.group2()
+        #         g_errors = self.errors_group2()
+        #
+        #     self.channels[-group_id] = sp.mean(group, axis=0)
+        #     err = np.zeros((1, len(g_errors[0])))
+        #     for i in range(len(g_errors)):
+        #         err += np.divide(g_errors[i]/group[i])**2
+        #     self('errors')[-group_id] = np.sqrt(err)
+        #
+        #     return self.channels[-group_id]
+        #
+        # def median(self, group_id):
+        #     if group_id > 2:
+        #         warnings.warn("group_id must be 1 or 2 only. Group 2 will be used as default.")
+        #         group = self.group2()
+        #         g_errors = self.errors_group2()
+        #     elif group_id == 1:
+        #         group = self.group1()
+        #         g_errors = self.errors_group1()
+        #     else:
+        #         group = self.group2()
+        #         g_errors = self.errors_group2()
+        #
+        #     self.channels[-group_id] = sp.median(group, axis=0)
+        #     err = np.zeros((1, len(g_errors[0])))
+        #     for i in range(len(g_errors)):
+        #         err += np.divide(g_errors[i]/group[i])**2
+        #     self.errors[-group_id] = np.sqrt(err)
+        #
+        #     return self.channels[-group_id]
+        #
