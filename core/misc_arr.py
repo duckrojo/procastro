Alllow querying header in a single string separated by commas. Fixed unclosed pyfits files. Add units to gain and ron. Cache entire header. Debug filter's _ commands. Includes a Commented AstroCalib and a line in  AstroFile.reader() that prepares for CCDData. Use correct pyfits dependence

core/misc_arr.py
@@ -22,7 +22,7 @@ from __future__ import print_function, division
 import scipy as sp
 import inspect
 import scipy.signal as sg
-import pyfits as pf
+import astropy.io.fits as pf
 import copy
 from IPython.core.debugger import Tracer
 