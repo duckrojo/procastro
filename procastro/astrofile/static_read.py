997f5f384020b821406562d0aeae1ec193eee38f
duckrojo <3063609+duckrojo@users.noreply.github.com>
2025-01-29 23:42:39 +0800

    can correctly read multi-epoch single-file timeseries file

--- procastro/astrofile/static_read.py
+++ procastro/astrofile/static_read.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 from astropy.io import fits as pf
 from astropy.table import Table
+from numpy.ma.core import MaskedArray
 
 
 def static_read(file_type,
@@ -43,11 +44,11 @@ def static_ndarray_to_table(data, file_options=None):
 
         if n_axes == 1:
             data = data.reshape(1, nx)
-        else:
-            for remove_ax in range((n_axes - 2 > 0) * (n_axes - 2)):
-                data = data[0]
+        # else:
+        #     for remove_ax in range((n_axes - 2 > 0) * (n_axes - 2)):
+        #         data = data[0]
 
-        n_channels = data.shape[0]
+        n_channels = data.shape[len(data.shape) - 2]
 
         try:
             column_names = file_options['colnames']
@@ -55,8 +56,10 @@ def static_ndarray_to_table(data, file_options=None):
             column_names = [f"{i}" for i in range(n_channels)]
 
         table = Table()
-        for name, column in zip(column_names, data):
-            table[name] = column
+        axis = len(data.shape) - 2
+        for idx, name in enumerate(column_names):
+            data_channel = np.take(data, idx, axis=axis).transpose()
+            table[name] = MaskedArray(data_channel, mask=np.isnan(data_channel))
         return table
     else:
         raise TypeError(f"data type not supported: {type(data)}")
